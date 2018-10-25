3368f2cbbc525c6c326118d03c6b4c0e538f2a4d
Clóvis Neto <cloves13@hotmail.com>
2018-10-25 17:02:18 -0300

    Retrieve logged user instagram information

--- index.js
+++ index.js
@@ -1,37 +1,69 @@
 require('dotenv').config();
 
-var http = require('http');
-var express = require('express');
-var api = require('instagram-node').instagram();
-var app = express();
+const REDIRECT_URL = 'http://localhost:3000/redirect';
+const http = require('http');
+const express = require('express');
+const api = require('instagram-node').instagram();
+const request = require('request');
+const app = express();
+
+const sendError = (res, message) => {
+    res.status(500).send({
+        status: 'error',
+        message
+    })
+}
+
+const predict = (res, images) => res.status({
+    status: 'success'
+})
 
 api.use({
     client_id: process.env.client_id,
     client_secret: process.env.client_secret
 });
 
-var redirect_uri = 'http://localhost:3000/redirect';
 
 app.get('/auth', (req, res) => {
-    res.redirect(api.get_authorization_url(redirect_uri, {
+    res.redirect(api.get_authorization_url(REDIRECT_URL, {
         scope: ['basic', 'public_content'],
         state: '' 
     }));
 });
 
 app.get('/redirect', (req, res) => {
-    api.authorize_user(req.query.code, redirect_uri, function(err, result) {
+    api.authorize_user(req.query.code, REDIRECT_URL, function(err, {access_token}) {
         if (err) {
             console.log(err.body);
             res.send("Didn't work");
         }
         else {
-            var token = result.access_token;
-            console.log('Access token is ' + token);
-            api.use({ access_token: token });
-            res.redirect('http://localhost:8080/?access_token=' + token);
+            res.redirect(`http://localhost:8080/?access_token=${access_token}`);
         }
     });
+});
+
+app.get('/make-prediction', (req, res) => {
+    const {access_token} = req.query;
+
+    if(!access_token) {
+        return sendError(res, 'You need to inform a valid access token');
+    }
+
+    request(
+        `https://api.instagram.com/v1/users/self/media/recent/?access_token=${access_token}`, 
+        (error, response, body)=> {
+            body = JSON.parse(body);
+
+            if(body.meta && body.meta.code == 400) {
+               return sendError(res, body.meta.error_message);
+            }
+            
+            const images = body.data.map(media => media.images.standard_resolution.url);
+
+            return predict(images);
+        }
+    )
 });
  
 http.createServer(app).listen(3000, function(){
